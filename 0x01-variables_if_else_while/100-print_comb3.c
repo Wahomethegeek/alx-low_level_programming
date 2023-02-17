729963b07e1151d66115d199e942f1b5b16a9f36
Wahomethegeek <gatwirikelvin@outlook.com>
2023-02-18 00:39:18 +0300

    Using brains

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all posisble combinations of two different digits in an ascending order
+ * main - Prints all posisble combinations of two different digits
  * Return: 0
  */
 int main(void)
@@ -13,7 +13,7 @@ int main(void)
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
-			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
 			if (num1 == 8 && num2 == 9)
 				continue;
