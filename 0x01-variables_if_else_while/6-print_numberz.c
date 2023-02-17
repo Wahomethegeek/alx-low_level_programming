23c0c1131acdd1b0a5be342ad57f62b0197b23fa
Wahomethegeek <gatwirikelvin@outlook.com>
2023-02-18 00:12:19 +0300

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers with the putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
