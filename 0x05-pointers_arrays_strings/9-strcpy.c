fa0a202375e3012f47dac1958eeb49abac063cb3
Wahomethegeek <gatwirikelvin@outlook.com>
2023-02-28 02:26:27 +0300

    correction

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,10 +7,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int b;
+	int a;
 
-	for (b = 0; src[b] != '\0'; b++)
-		dest[b] = src[b];
-	dest[b] = '\0';
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
 	return (dest);
 }
