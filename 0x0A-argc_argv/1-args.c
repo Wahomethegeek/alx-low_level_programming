bd405db8b8c688ffad928d99ba0457ef3a9a5757
Wahomethegeek <gatwirikelvin@outlook.com>
2023-03-12 23:23:06 +0300

    learning more about argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments passed
+ * @argc: number of command arguments
+ * @argv: size of array argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
