f90b02f2436f538f842a60aab1a0feab2540acd4
Wahomethegeek <gatwirikelvin@outlook.com>
2023-03-17 22:59:03 +0300

    concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int x = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s2[len1])
+	while (s1 && s1[len1])
 		len1++;
 
 	while (s2 && s2[len2])
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			x++;
 	}
 
-	while (n >= len2 && x < (len1 + len2))
+	while (n >= len2 && x < (len1 + n))
 		s[x++] = s2[j++];
 
 	while (n >= len2 && x < (len1 + len2))
