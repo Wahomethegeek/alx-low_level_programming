46782a615d66c057c3d8b91d17fcc791625af507
Wahomethegeek <gatwirikelvin@outlook.com>
2023-07-05 01:40:25 +0300

    adding functionality of ptinting the list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdef.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,7 +14,7 @@
 typedef struct listint_s
 {
 	int n;
-	struct listint_s *next
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
