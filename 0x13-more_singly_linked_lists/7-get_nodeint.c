60c15b070dcd8493f9a37414968e6bb980f6caf6
Wahomethegeek <gatwirikelvin@outlook.com>
2023-07-05 04:01:25 +0300

    fixing errors

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,7 +6,7 @@
  * @index: The data the node returns
  * Return: pointing to the node in return
  */
-listint_t get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
