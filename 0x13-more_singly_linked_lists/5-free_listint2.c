bbaf7778ce7bbcc0bff80b87c41f81418a27c6b3
Wahomethegeek <gatwirikelvin@outlook.com>
2023-07-05 03:44:38 +0300

    fixing betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,7 +6,7 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *count
+	listint_t *count;
 
 	if (head == NULL)
 	return;
@@ -15,7 +15,7 @@ void free_listint2(listint_t **head)
 	{
 		count = (*head)->next;
 		free(*head);
-		*head = temp;
+		*head = count;
 	}
 
 	*head = NULL;
